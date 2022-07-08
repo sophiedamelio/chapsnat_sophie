9635108007d0f4184dc460ee83d53d9839d960a2
sophiedamelio <sophiedamelio@gmail.com>
2022-07-07 22:07:34 -0700

    Install gifted chat, impliment basic version

--- App.js
+++ App.js
@@ -1,13 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { GiftedChat } from 'react-native-gifted-chat'
+
 
 export default function App() {
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    setMessages([
+      {
+        _id: 1,
+        text: 'Hello developer',
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: 'React Native',
+          avatar: 'https://placeimg.com/140/140/any',
+        }
+      }
+    ])
+  }, [])
+
+  const onSend = useCallback((messages = []) => {
+    setMessages(previousMessages => GiftedChat.append(previousMessages, messages))
+  }, [])
+
   return (
-    <View style={styles.container}>
-      <Text>Helloooo up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    //<View style={styles.container}>
+    //  <Text>Helloooo up App.js to start working on your app!</Text>
+    //  <StatusBar style="auto" />
+    //</View>
+    <GiftedChat
+      messages={messages}
+      onSend={messages => onSend(messages)}
+      user={{
+        _id:1
+      }}
+      />
   );
 }
 
