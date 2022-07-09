5e3a6b96915bd904a072fc68946330b17f80fa04
sophiedamelio <sophiedamelio@gmail.com>
2022-07-08 17:48:58 -0700

    Experiement with retriving all docs in collection chats, not currently functional

--- App.js
+++ App.js
@@ -5,17 +5,36 @@ import { StyleSheet, Text, View } from 'react-native';
 import { GiftedChat } from 'react-native-gifted-chat'
 
 
-export default function App() {
+export default function App({ route }) {
   const [messages, setMessages] = useState([]);
+  //const [chatDoc, setChatDoc] = useState(route.params.chatDoc);
+  //const { chatName, currUser } = route.params;
+
+  async function fetchMessages(){
+    const chatsRef = db.collection('Chats');
+    const snapshot = await chatsRef.get();
+    console.log(snapshot, "<----snapshot")
+    let newMessages = snapshot.forEach(doc => {
+      console.log(doc.id, "=>", doc.data())
+      return doc.data();
+    })
+    setMessages(newMessages);
+  }
 
   useEffect(() => {
-    db.collection("Chats")
-    .doc("mysecondchat")
-    .get()
-    .then((snapshot) => {
-      console.log(snapshot.id);
-      console.log(snapshot.data());
-    });
+    fetchMessages();
+    //db.collection("Chats")
+    //.doc("mysecondchat")
+    //.get()
+    //.then((snapshot) => {
+    //  let newMessages = snapshot.data().messages.map((singleMessage) => {
+    //    singleMessage.createdAt = singleMessage.createdAt.seconds * 1000;
+    //    return singleMessage;
+    //  });
+      //console.log(snapshot.id);
+      //console.log(snapshot.data().messages, "<-- data");
+      //setMessages(newMessages);
+    //});
   }, []);
 
   const onSend = useCallback((messages = []) => {
