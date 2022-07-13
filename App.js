35020a29a7d4d625d8fac4e21e2ddc3664f9e9bb
sophiedamelio <sophiedamelio@gmail.com>
2022-07-13 09:41:41 -0700

    Stack nav lab, finished part B

--- App.js
+++ App.js
@@ -1,65 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import db from "./firebase";
-import firebase from "@firebase/app"; 
+import React from "react";
+import { StyleSheet } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import ChatScreen from "./screens/ChatScreen";
+import HomeScreen from "./screens/HomeScreen";
 
-import React, { useState, useEffect, useCallback } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { GiftedChat } from 'react-native-gifted-chat'
+const Stack = createStackNavigator();
 
-
-export default function App() {
-  const [messages, setMessages] = useState([]);
-
-  useEffect(() => {
-    let unsubscribeFromNewSnapshots = db
-      .collection("Chats")
-      .doc("myfirstchat")
-      .onSnapshot((snapshot) => {
-        console.log("New Snapshot!");
-        setMessages(snapshot.data().messages);
-      });
-  
-    return function cleanupBeforeUnmounting() {
-      unsubscribeFromNewSnapshots();
-    };
-  }, []);
-
-  const onSend = useCallback((messages = []) => {
-    db.collection("Chats")
-    .doc("myfirstchat")
-    .update({
-        // arrayUnion appends the message to the existing array
-        messages: firebase.firestore.FieldValue.arrayUnion(messages[0]),
-    });
-    setMessages((previousMessages) =>
-    GiftedChat.append(previousMessages, messages)
-    );
-  }, []);
-
-
+function App() {
   return (
-    <GiftedChat
-      messages={messages}
-      onSend={messages => onSend(messages)}
-      user={{
-        // current "blue bubble" user
-        _id: "1",
-        name: 'Sophie',
-        avatar: "https://i.imgur.com/8C9wiYe.png"
-      }}
-      placeholder={"please type something here..."}
-      showUserAvatar={true}
-      alwaysShowSend={true}
-      renderUsernameOnMessage={true}
-      />
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Chat" component={ChatScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
   },
 });
+
+export default App;
