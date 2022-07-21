c47a1740102272500c6368699768bad6c6b82eab
Baker Sharp <bakers@stanford.edu>
2022-07-21 14:38:05 -0700

    cleaned up signupscreen for student answer ref

--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -1,114 +1,116 @@
-import { Text, View, TextInput, StyleSheet, TouchableOpacity } from 'react-native';
+import { useState } from "react";
+import {
+  Text,
+  View,
+  TextInput,
+  StyleSheet,
+  TouchableOpacity,
+} from "react-native";
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 import { doc, setDoc } from "firebase/firestore";
 import db from "../firebase";
-import {useState} from "react"
 
+export default function LoginScreen({ navigation }) {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
 
-export default function LoginScreen({navigation}) {
-	const [email, setEmail] = useState();
-	const [password, setPassword] = useState();
+  const auth = getAuth();
 
-	const auth = getAuth();
-
-	async function handleSubmit() {
-		console.log("handle submit envoked!!")
-
-		createUserWithEmailAndPassword(auth, email, password)
-		.then((userCredential) => {
-      console.log("making a new user")
-			const user = userCredential.user;
-			auth.currentUser = user;
-      
-      setDoc(doc(db, "Users", user.uid), {
-        username: user.email,
-        bio: ""
-      }).then( arg => {
-        console.log("finished setDoc", arg)
+  async function handleSubmit() {
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        auth.currentUser = user;
+        console.log("making a new user on firestroe");
+        setDoc(doc(db, "Users", user.uid), {
+          // make sure to change these to match the fields on your firestore!
+          username: user.email,
+          bio: "",
+        });
+      })
+      .catch((error) => {
+        console.log("Error when signing up new user:".error);
       });
-      console.log("called setDoc async")
-		})
-		.catch((error) => {
-			const errorCode = error.code;
-			const errorMessage = error.message;
-		});
+  }
 
-    
-	}
+  return (
+    <>
+      <Text style={styles.bigBlue}>Signup Here</Text>
+      <View style={styles.inputView}>
+        <TextInput
+          placeholder="Email"
+          placeholderTextColor="#003f5c"
+          onChangeText={(email) => setEmail(email)}
+        />
+      </View>
+      <View style={styles.inputView}>
+        <TextInput
+          placeholder="Password"
+          secureTextEntry={true}
+          placeholderTextColor="#003f5c"
+          onChangeText={(password) => setPassword(password)}
+        />
+      </View>
 
-	return (
-		<>
-			<Text style={styles.bigBlue}>Signup Here</Text>
-			<View style={styles.inputView}>
-				<TextInput
-					placeholder='Email'
-					placeholderTextColor="#003f5c"
-					onChangeText={(email) => setEmail(email)}
-				/>
-			</View>
-			<View style={styles.inputView}>
-				<TextInput
-					placeholder='Password'
-					secureTextEntry={true}
-					placeholderTextColor="#003f5c"
-					onChangeText={(password) => setPassword(password)}
-				/>
-			</View>
-			
-			<TouchableOpacity style={styles.loginBtn} onPress={() => {
-				handleSubmit();
-			}}>
-				<Text style={styles.loginText}>Signup</Text>
-			</TouchableOpacity>
+      <TouchableOpacity
+        style={styles.loginBtn}
+        onPress={() => {
+          handleSubmit();
+        }}
+      >
+        <Text style={styles.loginText}>Signup</Text>
+      </TouchableOpacity>
 
-{/*  */}
-			<TouchableOpacity style={styles.redirectBtn} onPress={() => {
-				navigation.navigate("Login")
-			}}>
-				<Text>Already have an account? Login here</Text>
-			</TouchableOpacity>
-		</>
-	)
+      <TouchableOpacity
+        style={styles.redirectBtn}
+        onPress={() => {
+          navigation.navigate("Login");
+        }}
+      >
+        <Text>Already have an account? Login here</Text>
+      </TouchableOpacity>
+    </>
+  );
 }
 
 const styles = StyleSheet.create({
-	redirectBtn: {
-		width:"80%",
-		borderRadius:25,
-		height:50,
-		alignItems:"center",
-		justifyContent:"center",
-		marginTop:40,
-		backgroundColor:"grey",
-		color: "white"
-	},
-	inputView: {
-		backgroundColor: "#FFC0CB",
-		borderRadius: 30,
-		width: "70%",
-		height: 45,
-		marginBottom: 20,
-		alignItems: "center",
-	},
-	TextInput: {
-		height: 50,
-		flex: 1,
-		padding: 10,
-		marginLeft: 20,
-	},
-	loginBtn: {
-		width:"80%",
-		borderRadius:25,
-		height:50,
-		alignItems:"center",
-		justifyContent:"center",
-		marginTop:40,
-		backgroundColor:"#FF1493",
-	},
-	bigBlue: {
-		color: 'blue',
-		fontWeight: 'bold',
-		fontSize: 30,
-		padding: 50
-	}
-})
+  redirectBtn: {
+    width: "80%",
+    borderRadius: 25,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 40,
+    backgroundColor: "grey",
+    color: "white",
+  },
+  inputView: {
+    backgroundColor: "#FFC0CB",
+    borderRadius: 30,
+    width: "70%",
+    height: 45,
+    marginBottom: 20,
+    alignItems: "center",
+  },
+  TextInput: {
+    height: 50,
+    flex: 1,
+    padding: 10,
+    marginLeft: 20,
+  },
+  loginBtn: {
+    width: "80%",
+    borderRadius: 25,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 40,
+    backgroundColor: "#FF1493",
+  },
+  bigBlue: {
+    color: "blue",
+    fontWeight: "bold",
+    fontSize: 30,
+    padding: 50,
+  },
+});
